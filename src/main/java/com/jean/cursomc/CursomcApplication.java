03659d8fbbdbfd40dc895f834558fabd5fc232ce
Jean Crus <jean.carlos@pucpr.edu.br>
2019-04-26 12:04:59 -0300

    Cliente, TipoCliente, telefones(weak association) e Enderecos

--- src/main/java/com/jean/cursomc/CursomcApplication.java
+++ src/main/java/com/jean/cursomc/CursomcApplication.java
@@ -9,10 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.jean.cursomc.domain.Categoria;
 import com.jean.cursomc.domain.Cidade;
+import com.jean.cursomc.domain.Cliente;
+import com.jean.cursomc.domain.Endereco;
 import com.jean.cursomc.domain.Estado;
 import com.jean.cursomc.domain.Produto;
+import com.jean.cursomc.domain.enums.TipoCliente;
 import com.jean.cursomc.repositories.CategoriaRepository;
 import com.jean.cursomc.repositories.CidadeRepository;
+import com.jean.cursomc.repositories.ClienteRepository;
+import com.jean.cursomc.repositories.EnderecoRepository;
 import com.jean.cursomc.repositories.EstadoRepository;
 import com.jean.cursomc.repositories.ProdutoRepository;
 
@@ -31,6 +36,13 @@ public class CursomcApplication implements CommandLineRunner {
 	@Autowired
 	private EstadoRepository estadoRepository;
 
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
+	@Autowired
+	private EnderecoRepository enderecoRepository;
+
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
@@ -66,6 +78,19 @@ public class CursomcApplication implements CommandLineRunner {
 		
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+		
+		Cliente cli1 = new Cliente (null, "Maria Silva", "maria@gmail.com", "36378912377", TipoCliente.PESSOAFISICA); 
+		//TipoCliente.PESSOAFISICA pois o mesmo é um enum  
+		cli1.getTelefones().addAll(Arrays.asList("27363323","93838393"));
+		
+		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardim", "38220834", cli1, c1);
+		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
+		
+		cli1.getEnderecos().addAll(Arrays.asList(e1,e2));
+		
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		enderecoRepository.saveAll(Arrays.asList(e1,e2));
+		
 		
 	}
 
---
+++ src/main/java/com/jean/cursomc/domain/Cliente.java
@@ -0,0 +1,143 @@
+package com.jean.cursomc.domain;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import javax.persistence.CollectionTable;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import com.jean.cursomc.domain.enums.TipoCliente;
+
+@Entity
+public class Cliente implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String nome;
+	private String email;
+	private String cpfOuCnpj;
+	private Integer tipo; //tipo do TipoCliente foi alterado para INTEGER porém o mundo verá ele como tipocliente
+	
+	@OneToMany(mappedBy = "cliente")
+	private List<Endereco> enderecos = new ArrayList<>();
+	
+	/*
+	 * Através do set(propriedade da classe) garante que o conjunto de strings não terá
+	 * repetição, assim podendo fazer vários numeros sem serem repetidos
+	 */
+	@ElementCollection //setar no banco telefones como entidade fraca
+	@CollectionTable(name = "TELEFONE") //criar tabela auxiliar no banco
+	private Set<String> telefones = new HashSet<>(); //conjunto de string
+
+	public Cliente() {
+	}
+
+	public Cliente(Integer id, String nome, String email, String cpfOuCnpj, TipoCliente tipo) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.email = email;
+		this.cpfOuCnpj = cpfOuCnpj;
+		this.tipo = tipo.getCod();
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getCpfOuCnpj() {
+		return cpfOuCnpj;
+	}
+
+	public void setCpfOuCnpj(String cpfOuCnpj) {
+		this.cpfOuCnpj = cpfOuCnpj;
+	}
+	
+	/*
+	 * get e set do TIPOCLIENTE alterado para armazenar internamente 
+	 * um numero inteiro controlado por nós no qual é feito um numero inteiro
+	 * entretanto, expoem para o sistema o dado do tipocliente
+	 */
+	public TipoCliente getTipo() {
+		return TipoCliente.toEnum(tipo);
+	}
+
+	public void setTipo(TipoCliente tipo) {
+		this.tipo = tipo.getCod();
+	}
+
+	public List<Endereco> getEnderecos() {
+		return enderecos;
+	}
+
+	public void setEnderecos(List<Endereco> enderecos) {
+		this.enderecos = enderecos;
+	}
+
+	public Set<String> getTelefones() {
+		return telefones;
+	}
+
+	public void setTelefones(Set<String> telefones) {
+		this.telefones = telefones;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
+	
+}
