7f7fed2f5cff8ebe5fc05fec041f229a2a595b5e
Jean Crus <jean.carlos@pucpr.edu.br>
2019-04-30 20:46:54 -0300

    Validacao customizada: email nao repetido na atualizacao de Cliente

---
+++ src/main/java/com/jean/cursomc/services/validation/ClientUpdateValidator.java
@@ -0,0 +1,57 @@
+package com.jean.cursomc.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerMapping;
+
+import com.jean.cursomc.domain.Cliente;
+import com.jean.cursomc.domain.enums.TipoCliente;
+import com.jean.cursomc.dto.ClienteDTO;
+import com.jean.cursomc.dto.ClienteNewDTO;
+import com.jean.cursomc.repositories.ClienteRepository;
+import com.jean.cursomc.resources.exception.FieldMessage;
+import com.jean.cursomc.services.validation.utils.BR;
+
+public class ClientUpdateValidator implements ConstraintValidator<ClienteUpdate, ClienteDTO> {
+	
+	@Autowired
+	private HttpServletRequest request; //tem função para obter o parametro da URI
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	@Override
+	public void initialize(ClienteUpdate ann) {
+	}
+
+	@Override
+	public boolean isValid(ClienteDTO objDto, ConstraintValidatorContext context) {
+		
+		//utilizando map pois a requisição pode conter vários atributos e esses atributos são guardados dentro de um MAP e precisamos pegar a chave id e o valor
+		@SuppressWarnings("unchecked")
+		Map<String, String> map = (Map<String, String>) request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+		Integer uriId = Integer.parseInt(map.get("id"));
+		
+		List<FieldMessage> list = new ArrayList<>();
+	
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		
+		if (aux != null && ! aux.getId().equals(uriId)) {
+			list.add(new FieldMessage("email", "E-mail já existe"));
+		}
+		
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+}
