b16d8a8dc0f0a98cde164cd15779705b803e7cf7
Jean Crus <jean.carlos@pucpr.edu.br>
2019-04-29 23:00:21 -0300

    Inserindo um novo Cliente com POST

--- src/main/java/com/jean/cursomc/resources/ClienteResource.java
+++ src/main/java/com/jean/cursomc/resources/ClienteResource.java
@@ -1,5 +1,6 @@
 package com.jean.cursomc.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,15 +9,18 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.jean.cursomc.domain.Cliente;
 import com.jean.cursomc.dto.ClienteDTO;
+import com.jean.cursomc.dto.ClienteNewDTO;
 import com.jean.cursomc.services.ClienteService;
 
 @RestController
@@ -30,6 +34,16 @@ public class ClienteResource {
 	public ResponseEntity<Cliente> find(@PathVariable Integer id) {
 		Cliente obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@Transactional
+	@RequestMapping(method = RequestMethod.POST) //mapeado para que seja reconhecido no endpoint categorias
+	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteNewDTO objDto) { //request body pois necessário para que o json seja reconhecido automaticamente para o java
+		Cliente obj = service.fromDTO(objDto);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest() //Sendo feito para que a / após a categoria seja acrescentada o id novo inserido
+				.path("/{id}").buildAndExpand(obj.getId()).toUri(); //do banco de dados 
+		return ResponseEntity.created(uri).build();
 	}
 	
 	@RequestMapping(value = "/{id}", method = RequestMethod.PUT) //atualizar
@@ -70,4 +84,6 @@ public class ClienteResource {
 		Page<ClienteDTO> listDto = list.map(obj -> new ClienteDTO(obj)); //por ser java 8, o page não precisa de collectors e stream
 		return ResponseEntity.ok().body(listDto);
 	}
+	
+	
 }
--- src/main/java/com/jean/cursomc/services/ClienteService.java
+++ src/main/java/com/jean/cursomc/services/ClienteService.java
@@ -10,9 +10,14 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.jean.cursomc.domain.Cidade;
 import com.jean.cursomc.domain.Cliente;
+import com.jean.cursomc.domain.Endereco;
+import com.jean.cursomc.domain.enums.TipoCliente;
 import com.jean.cursomc.dto.ClienteDTO;
+import com.jean.cursomc.dto.ClienteNewDTO;
 import com.jean.cursomc.repositories.ClienteRepository;
+import com.jean.cursomc.repositories.EnderecoRepository;
 import com.jean.cursomc.services.exceptions.DataIntegrityException;
 import com.jean.cursomc.services.exceptions.ObjectNotFoundException;
 
@@ -21,12 +26,21 @@ public class ClienteService {
 	
 	@Autowired
 	private ClienteRepository repo;
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 	
 	public Cliente find(Integer id) {
 		Optional<Cliente> obj = repo.findById(id); // se existe retorna obj else retorna null
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Cliente não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
 		}
+	
+	public Cliente insert(Cliente obj) {
+		obj.setId(null); //garante que o objeto inserido é novo(nulo), pois se não o insert é considerado como atualização e muda o obj ja criado
+		obj = repo.save(obj);
+		enderecoRepository.saveAll(obj.getEnderecos());
+		return obj;
+	}
 	
 	public Cliente update(Cliente obj) {
 		Cliente newObj = find(obj.getId()); //procura o obj cliente.id e insere no newobj
@@ -59,8 +73,26 @@ public class ClienteService {
 		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null); //não instancia do banco de dados
 	}
 	
+	public Cliente fromDTO(ClienteNewDTO objDto) {
+		Cliente cli = new Cliente(null, objDto.getNome(), objDto.getEmail(), objDto.getCpfOuCnpj(), TipoCliente.toEnum(objDto.getTipo()));
+		Cidade cid = new Cidade(objDto.getCidadeId(), null, null);
+		Endereco end = new Endereco(null,objDto.getLogradouro(), objDto.getNumero(), objDto.getNumero(), objDto.getBairro(), objDto.getCep(), cli , cid);
+		cli.getEnderecos().add(end);
+		cli.getTelefones().add(objDto.getTelefone1());
+		if (objDto.getTelefone2() != null) {
+			cli.getTelefones().add(objDto.getTelefone2());
+		}
+		if (objDto.getTelefone3() != null) {
+			cli.getTelefones().add(objDto.getTelefone3());
+		}
+		
+		return cli;
+	}
+	
 	private void updateData(Cliente newObj, Cliente obj) {
 		newObj.setNome(obj.getNome());
 		newObj.setEmail(obj.getEmail());
 	}// novo metodo inserido para que somente o email e nome sejam alterados
+	
+	
 }
