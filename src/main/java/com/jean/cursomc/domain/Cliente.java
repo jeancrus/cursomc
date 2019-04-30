9a8fa8c616e7bf3bfd162fc1c94338c10b9fc7a4
Jean Crus <jean.carlos@pucpr.edu.br>
2019-04-29 23:32:51 -0300

    Ajustando e testando a delecao de Cliente

--- src/main/java/com/jean/cursomc/domain/Cliente.java
+++ src/main/java/com/jean/cursomc/domain/Cliente.java
@@ -6,6 +6,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.CollectionTable;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
@@ -31,7 +32,7 @@ public class Cliente implements Serializable {
 	private Integer tipo; //tipo do TipoCliente foi alterado para INTEGER porém o mundo verá ele como tipocliente
 	
 	
-	@OneToMany(mappedBy = "cliente")
+	@OneToMany(mappedBy = "cliente", cascade = CascadeType.ALL) //regra de negócio, controle de poder deletar cliente e seus endereços
 	private List<Endereco> enderecos = new ArrayList<>();
 	
 	/*
