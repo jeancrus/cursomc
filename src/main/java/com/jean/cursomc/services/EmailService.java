e0cc9fb81f959ae1293f79b5f18d8aa420934b4f
Jean Crus <jean.carlos@pucpr.edu.br>
2019-05-02 21:02:38 -0300

    Email HTML

--- src/main/java/com/jean/cursomc/services/EmailService.java
+++ src/main/java/com/jean/cursomc/services/EmailService.java
@@ -1,5 +1,7 @@
 package com.jean.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.jean.cursomc.domain.Pedido;
@@ -7,6 +9,11 @@ import com.jean.cursomc.domain.Pedido;
 public interface EmailService {
 	
 	void sendOrderConfirmationEmail(Pedido obj);
+	
 	void sendEmail(SimpleMailMessage msg);
+	
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+	
+	void sendHtmlEmail(MimeMessage msg);
 
 }
